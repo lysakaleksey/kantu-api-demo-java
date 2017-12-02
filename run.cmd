3e4ac41b158153783b2ce692860d578a16d4a163
Aleksey Lysak <lysakaleksey@restrixio.com>
2017-12-02 09:45:15 +0200

    Minor update in scripts

--- run.cmd
+++ run.cmd
@@ -1,3 +1,4 @@
-set libs="%cd%\dist\libs\ant-1.7.0.jar;%cd%\dist\libs\ant-launcher-1.7.0.jar;%cd%\dist\libs\asm-3.0.jar;%cd%\dist\libs\asm-commons-3.0.jar;%cd%\dist\libs\asm-tree-3.0.jar;%cd%\dist\libs\forms-1.1-preview.jar;%cd%\dist\libs\forms_rt-7.0.3.jar;%cd%\dist\libs\jacob-1.14.3.jar;%cd%\dist\libs\javac2-7.0.3.jar;%cd%\dist\libs\jdom-1.0.jar;%cd%\dist\kantu-api-demo-com-1.0-SNAPSHOT.jar"
-"%JAVA_HOME%\bin\java" -Djava.library.path=%cd%\dist\dlls -cp "%libs%" com.kantu.demo.MainApp
-
+set dist="%cd%\dist"
+set libs="%dist%\libs"
+set classpath="%libs%\ant-1.7.0.jar;%libs%\ant-launcher-1.7.0.jar;%libs%\asm-3.0.jar;%libs%\asm-commons-3.0.jar;%libs%\asm-tree-3.0.jar;%libs%\forms-1.1-preview.jar;%libs%\forms_rt-7.0.3.jar;%libs%\jacob-1.14.3.jar;%libs%\javac2-7.0.3.jar;%libs%\jdom-1.0.jar;%dist%\kantu-api-demo-com-1.0-SNAPSHOT.jar"
+"%JAVA_HOME%\bin\java" -Djava.library.path=%cd%\dist\dlls -classpath "%classpath%" com.kantu.demo.MainApp
